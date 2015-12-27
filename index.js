541e85716a62d0a7cfab7ff7498d56eea4215eb5
Gean <gean.marroquin@gmail.com>
2015-12-26 22:19:48 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+var cached = require('gulp-cached')
+var changed = require('gulp-changed')
+var Elixir = require('laravel-elixir')
+var filter = require('gulp-filter')
+var gulp = require('gulp')
+var jade = require('gulp-jade')
+var jadeInheritance = require('gulp-jade-inheritance')
+var plumber = require('gulp-plumber')
+var rename = require('gulp-rename')
+var _ = require('underscore')
+
+/*
+ |----------------------------------------------------------------
+ | Gulp Jade Wrapper
+ |----------------------------------------------------------------
+ |
+ | This task will compile your Jade files that have changed.
+ | This also prevent partials from being processed separately
+ | by marking them with an underscore before their name.
+ | You can make use of Blade variables in your jade files as well.
+ |
+ */
+
+Elixir.extend('jade', function (src, output, options) {
+  options = _.extend({
+    basedir: 'resources/views', // required to use absolute paths
+    extension: '.html',
+    pretty: true
+  }, options)
+
+  var jade_options = _.pick(
+    options,
+    'filename',
+    'doctype',
+    'pretty',
+    'self',
+    'debug',
+    'compileDebug',
+    'compiler',
+    'basedir'
+  )
+
+  var gulp_src = options.basedir + '/' + src
+  var gulp_dest = output || 'public/views'
+
+  new Elixir.Task('jade', function () {
+    this.log(gulp_src, gulp_dest) // Log src and output
+
+    return gulp.src(gulp_src)
+      .pipe(plumber())
+      .pipe(changed(gulp_dest, { extension: options.extension }))
+      .pipe(cached('jade'))
+      .pipe(jadeInheritance({ basedir: options.basedir }))
+      .pipe(filter(function (file) {
+        return !/\/_/.test(file.path) && !/^_/.test(file.relative)
+      }))
+      .pipe(jade(jade_options))
+      .pipe(rename(function (path) {
+        path.extname = options.extension
+      }))
+      .pipe(gulp.dest(gulp_dest))
+      .pipe(new Elixir.Notification('Jade Compiled!'))
+  })
+  .watch(options.basedir + '/**/*.jade')
+})
---
+++ readme.md
@@ -0,0 +1,66 @@
+## Laravel Elixir Jade
+
+This Laravel Elixir extension allows you to compile [jade](https://github.com/pugjs/jade).
+
+### Goals
+
+* This task will compile your Jade files that have changed*.
+* This also prevent partials from being processed separately by marking them with an underscore before their name.
+
+### Installation
+
+First, pull in the extension through NPM.
+
+```sh
+$ npm install --save-dev wo-laravel-elixir-jade
+```
+
+Next, add it to your Elixir-enhanced Gulpfile, like so:
+
+```javascript
+var elixir = require('laravel-elixir');
+
+require('wo-laravel-elixir-jade');
+
+elixir(function(mix) {
+  mix.jade('**/*.jade', 'public/views');
+});
+```
+
+***NOTE: Jade files, with default options, should be in a `resources/views` folder. Make sure to create one!***
+
+### Usage
+
+Assuming you write...
+
+```javascript
+elixir(function(mix) {
+  mix.jade('**/*.jade', 'public/views');
+});
+```
+
+...this will compile yours `resources/views/**/*.jade` files to `public/views` folder.
+
+Finally, if you want to override the Jade plugin options, you may pass an object as the third argument.
+
+```javascript
+mix.jade('**/*.jade', 'public/views', {});
+
+// For Jade's options, see http://jade-lang.com/api/
+```
+
+### Options
+
+You can also set `extension: .blade.php` to compile to `*.blade.php` instead of `*.html`.
+
+All other options should be pretty straight forward.
+
+These are the default options:
+
+```javascript
+{
+    basedir: 'resources/views', // required to use absolute paths and default jade source folder
+    extension: '.html',
+    pretty: true
+}
+```
